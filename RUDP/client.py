1c0dd9f743e7f9f1cf218d053e714790b79529cf
hsiuyao2 <hsiuyao2@illinois.edu>
2019-11-17 14:30:45 -0600

    add multi-threading

--- RUDP/client.py
+++ RUDP/client.py
@@ -4,9 +4,13 @@ import collections
 import argparse
 import logging
 from datetime import datetime
-
+from threading import Thread
+from queue import Queue
+from copy import deepcopy
+import time 
 udp_data_port = 18888
-udp_missing_port = 18889
+udp_missing_send_port = 18889
+udp_missing_recv_port = 18890
 
 metadata_size = 16
 block_size = 1024
@@ -14,14 +18,42 @@ udp_receive_size = block_size + metadata_size
 
 check_missing_cnt = 100
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', default='localhost')
+args = parser.parse_args()
+
 class client:
     def __init__(self):
         self.final_dict = collections.OrderedDict()
         self.id_ptr = 0
+        self.missing_buffer = Queue()
+        self.recv_thread = Thread(target=self.receive_udp)
+        self.missing_send_thread = Thread(target=self.missing_udp_send)
+        self.missing_recv_thread = Thread(target=self.missing_udp_recv)
+        self.check_thread = Thread(target=self.check_all_recv)
+        self.isEnd = False
+
+    def run(self):
+        print("start recv_thread")
+        self.recv_thread.start()
+        self.missing_recv_thread.start()
+        
+        time.sleep(10)
+        print("start missing_send_thread")
+        self.missing_send_thread.start()
+
+        self.check_thread.start()
+
+
+        self.recv_thread.join()
+        self.missing_recv_thread.join()
+        self.missing_send_thread.join()
+        
+        self.check_all_recv.join()
 
     def receive_udp(self):
+        print("start listening")
         data_dict = {}
-        final_data = bytearray()
         data = bytearray()
         s = socket(AF_INET, SOCK_DGRAM)
         s.bind(('localhost', udp_data_port))
@@ -30,21 +62,22 @@ class client:
         while True:
             if counter%check_missing_cnt == 0:
                 self.check_missing(data_dict)
+                data_dict = {}
+
             payload, addr = s.recvfrom(udp_receive_size)
+
             if payload == b'':
-                logging.debug('UDP end recv')
+                #logging.debug('UDP end recv')
+                self.check_missing(data_dict)
+                print('UDP end recv')
                 s.close()
                 break
+
             start = int.from_bytes(payload[0:4], 'little')
             data = payload[16:]
             data_dict[start] = data
             counter += 1
-        return data_dict
 
-    def create_udp_socket(self):
-        s = socket(AF_INET, SOCK_DGRAM)
-        s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        return s
 
     def check_missing(self, data_dict):
         sorted_data_dict = sorted(data_dict.items())
@@ -52,20 +85,45 @@ class client:
             #print(id)
             while self.id_ptr < id:
                 print('data miss at id: {}'.format(self.id_ptr))
+                self.missing_buffer.put(self.id_ptr)
                 self.final_dict[self.id_ptr] = b''
                 self.id_ptr += 1
+
             self.final_dict[self.id_ptr] = block
             self.id_ptr += 1
 
-    def missing_udp_send(ids):
-        s = self.create_udp_socket()
-        for id in ids:
-            s.sendto(id, (args.host, udp_missing_port))
-            # send here
+    def missing_udp_send(self):
+        print("start missing send thread")
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        while True:
+            while not self.missing_buffer.empty():
+                missing_id = self.missing_buffer.get()
+                print("send missing id", missing_id)
+                s.sendto((missing_id).to_bytes(4, byteorder='little'), (args.host, udp_missing_send_port))
+
+    def missing_udp_recv(self):
+        print("start missing recv thread")
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.bind(('localhost', udp_missing_recv_port))
+        while True:
+            payload, addr = s.recvfrom(udp_receive_size)
+            start = int.from_bytes(payload[0:4], 'little')
+            print("recv missing id", start)
+            data = payload[16:]
+            self.final_dict[start] = data 
+
+    def check_all_recv(self):
+        while True:
+            time.sleep(1)
+            #print(self.final_dict.keys())
+            if list(self.final_dict.keys()) == list(range(431)) and all(self.final_dict.values()):
+                for k, v in self.final_dict.items():
+                    print(v)
+                break
+
 
 if __name__ == '__main__':
-    print("waiting the packet")
     c = client()
-    data = c.receive_udp()
-    #print(c.final_dict[0])
-    #print(c.final_dict[1])
+    c.run()
+
--- RUDP/server.py
+++ RUDP/server.py
@@ -5,17 +5,22 @@ from block import block
 from operator import attrgetter
 import time
 import collections
-import threading 
+from threading import Thread
+from queue import Queue
 
 from socket import socket, AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET, SOCK_DGRAM
 
 block_size = 1024
 expireTime = 3
 
-udp_missing_port = 18889
 udp_data_port = 18888
+udp_missing_send_port = 18890
+udp_missing_recv_port = 18889
 
-q = collections.OrderedDict()
+metadata_size = 16
+block_size = 1024
+udp_receive_size = block_size + metadata_size
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', default='localhost')
@@ -38,71 +43,88 @@ def getBlocks(fileName, step=1024):
         
     return blocks
 
-def create_tcp_socket():
-    s = socket(AF_INET, SOCK_STREAM)
-    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    s.bind((args.host, 16677))
-    s.listen(5)
-    return s
-
-
-def create_udp_socket():
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    return s
-
-
-def send_UDP(blocks):
-    isFinished = False
-    update_thread = threading.Thread(target=update, args=(lambda: isFinished,))
-    update_thread.start()
-
-    missing_thread = threading.Thread()
-
-    s = create_udp_socket()
-    for block in blocks:
-        block.timestamp = int(datetime.now().strftime('%s'))
-        block.build_meta()
-        q[block.id] = block
-        if block.id != 10 and block.id!=13:
-            s.sendto(block.metadata + block.data, (args.host, udp_data_port))
-        # send here
-    s.sendto(b'', (args.host, udp_data_port))
-    print('finish sending')
-    isFinished = True
-    update_thread.join()
-
-def missing_udp_recv(isFinished):
-    data = bytearray()
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.bind(('localhost', udp_missing_port))
-    data_dict = {}
-    while True:
-        payload, addr = s.recvfrom(udp_receive_size)
-        if payload == b'':
-            logging.debug('UDP missing end recv')
-            s.close()
-            break
+class Server:
+    def __init__(self, blocks):
+        self.blocks = blocks
+        self.sending_thread = Thread(target=self.send_UDP, args=(self.blocks,))
+        self.missing_recv_thread = Thread(target=self.missing_recv)
+        self.missing_send_thread = Thread(target=self.missing_send)
         
-    return data_dict
+        self.update_thread = Thread(target=self.update)
+        self.isFinished = False
 
-def update(isFinished):
-    while True:
-        if isFinished():
-            break
-        print('update')
-        curTime = int(datetime.now().strftime('%s'))
-        for key in list(q.keys()):
-            if q[key].timestamp >= curTime-expireTime:
+        self.missing_buffer = Queue()
+        self.q = collections.OrderedDict()
+
+    def run(self):
+        print("start missing_recv_thread")
+        self.missing_recv_thread.start()
+        time.sleep(10)
+
+        print("start sending_thread")
+        self.sending_thread.start()
+        self.update_thread.start()
+        self.missing_send_thread.start()
+
+        self.missing_recv_thread.join()
+        self.sending_thread.join()
+        self.update_thread.join()
+        self.missing_send_thread.join()
+
+    def send_UDP(self, blocks):
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+        for block in blocks:
+            block.timestamp = int(datetime.now().strftime('%s'))
+            block.build_meta()
+            self.q[block.id] = block
+            if block.id != 10 and block.id!=13:
+                s.sendto(block.metadata + block.data, (args.host, udp_data_port))
+            # send here
+        s.sendto(b'', (args.host, udp_data_port))
+        print('finish sending')
+        self.isFinished = True
+
+    def missing_recv(self):
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.bind(('localhost', udp_missing_recv_port))
+        while True:
+            payload, addr = s.recvfrom(udp_receive_size)
+            missing_block_id = int.from_bytes(payload[0:4], 'little')
+            print("recv missing block id", missing_block_id)
+            self.missing_buffer.put(missing_block_id)
+
+    def missing_send(self):
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        while True:
+            while not self.missing_buffer.empty():
+                missing_block_id = self.missing_buffer.get()
+                if missing_block_id in self.q:
+                    missing_block = self.q[missing_block_id]
+                    s.sendto(missing_block.metadata + missing_block.data, (args.host, udp_missing_send_port))
+
+    def update(self):
+        while True:
+            if self.isFinished:
                 break
-            del q[key]
-        time.sleep(3)
-        
+            print('update')
+            curTime = int(datetime.now().strftime('%s'))
+            for key in list(self.q.keys()):
+                if self.q[key].timestamp >= curTime-expireTime:
+                    break
+                del self.q[key]
+            time.sleep(3)
 
 if __name__ == '__main__':
+    print("Generating blocks")
     blocks = getBlocks("data/novel.txt", block_size)
-    send_UDP(blocks)
-
+    #send_UDP(blocks)
+    print("# of Blocks", len(blocks))
+    server = Server(blocks)
+    server.run()
+    
     
     '''
     for block in blocks:
